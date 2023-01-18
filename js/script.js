fb73fedb8db60a2cd727800035e27fc67dc622e0
Marian Simo <sim0302@vsb.cz>
2023-01-18 14:36:41 +0100

    added encrypted email msging in js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ window.onscroll = () =>{
     menu.classList.remove("bx-x");
     navbar.classList.remove("open");
 }
+
+/* https://smtpjs.com/ CA591BAF44820377DBB51891DCF9318ED5FF*/
+function sendEmail(){
+        Email.send({
+            SecureToken : "9a8b254f-ef5d-4441-bd12-a786857bcc6a",
+            To : 'simomarian99@gmail.com',
+            From : 'simomarian99@gmail.com',
+            Subject : document.getElementById("email").value,
+            Body : '<h1>' + document.getElementById("subject").value + '</h1>' + ' :' +  document.getElementById("message").value,
+        }).then(
+          message => alert(message)
+        );
+        console.log('EMAIL SENT');
+}
